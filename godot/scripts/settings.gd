cdccb79b700a1927964f40fdf3d56b3522e754cb
Kurt Schwind <kurt.schwind@gmail.com>
2025-09-12 16:30:20 -0500

    Settings2

--- godot/scripts/settings.gd
+++ godot/scripts/settings.gd
@@ -4,3 +4,7 @@ extends Control
 # Read Global Config
 func _ready() -> void:
 	pass # Replace with function body.
+
+
+func _on_select_directory_button_down() -> void:
+	$MarginContainer/DirectorySelector.popup()
