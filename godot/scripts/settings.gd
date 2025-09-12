7a3b0c5c9682352162b152a700f91d0b4175f481
Kurt Schwind <kurt.schwind@gmail.com>
2025-09-12 18:02:13 -0500

    Settings2

--- godot/scripts/settings.gd
+++ godot/scripts/settings.gd
@@ -1,10 +1,15 @@
 extends Control
 
+var verse_dir: String = "Select Directory"
 
 # Read Global Config
 func _ready() -> void:
-	pass # Replace with function body.
+	$MarginContainer/VBoxContainer/HBoxContainer/LineEdit.text = verse_dir
 
 
 func _on_select_directory_button_down() -> void:
 	$MarginContainer/DirectorySelector.popup()
+
+
+func _on_directory_selector_dir_selected(dir: String) -> void:
+	$MarginContainer/VBoxContainer/HBoxContainer/LineEdit.text = dir
