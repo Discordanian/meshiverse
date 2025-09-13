9784bcb6eec9931805e9843419f315e4a4d52a27
Kurt Schwind <kurt.schwind@gmail.com>
2025-09-13 05:45:04 -0500

    New Open Buttons

--- godot/scripts/settings.gd
+++ godot/scripts/settings.gd
@@ -4,12 +4,25 @@ var verse_dir: String = "Select Directory"
 
 # Read Global Config
 func _ready() -> void:
-	$MarginContainer/VBoxContainer/HBoxContainer/LineEdit.text = verse_dir
+	$MarginContainer/VBoxContainer/DisplayDirectory.text = verse_dir
+	$MarginContainer/OpenSelector.add_filter("meshiverse.ini","Meshiverse settings file")
 
 
 func _on_select_directory_button_down() -> void:
-	$MarginContainer/DirectorySelector.popup()
+	$MarginContainer/NewSelector.popup()
 
 
 func _on_directory_selector_dir_selected(dir: String) -> void:
-	$MarginContainer/VBoxContainer/HBoxContainer/LineEdit.text = dir
+	set_vault_directory(dir)
+
+
+func _on_open_button_down() -> void:
+	$MarginContainer/OpenSelector.popup()
+
+
+func _on_open_selector_file_selected(path: String) -> void:
+	set_vault_directory(path.get_base_dir())
+	
+func set_vault_directory(dir: String) -> void:
+	verse_dir = dir
+	$MarginContainer/VBoxContainer/DisplayDirectory.text = dir
