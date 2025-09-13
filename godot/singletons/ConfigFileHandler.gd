a0d9b93f3f37affe96476a1f57fbb38f40918178
Kurt Schwind <kurt.schwind@gmail.com>
2025-09-13 06:26:26 -0500

    Config

--- godot/singletons/ConfigFileHandler.gd
+++ godot/singletons/ConfigFileHandler.gd
@@ -2,12 +2,15 @@ extends Node
 
 
 var global_config = ConfigFile.new()
+var vault_config = ConfigFile.new()
+
+const APP_VERSION = "application/config/version"
 const GLOBAL_CONFIG_PATH = "user://settings.cfg"
 
 func _ready():
 	if !FileAccess.file_exists(GLOBAL_CONFIG_PATH):
 		print("Global Config does not exist.  Creating Default Config")
-		global_config.set_value("main","version",ProjectSettings.get_setting("application/config/version"))
+		global_config.set_value("main","version",ProjectSettings.get_setting(APP_VERSION))
 		
 		global_config.save(GLOBAL_CONFIG_PATH)
 	else:
@@ -17,6 +20,22 @@ func _ready():
 		else:
 			print("Configuration loaded")
 	print_config(global_config)
+
+func set_vault_dir(dir: String):
+	global_config.set_value("vault","path", dir)
+	global_config.save(GLOBAL_CONFIG_PATH)
+	
+
+func new_vault_dir(dir: String):
+	var vault_settings_file = dir.path_join("meshiverse.cfg")
+	if FileAccess.file_exists(vault_settings_file):
+		print("Error the settings file already exists")
+		return
+	vault_config = ConfigFile.new()
+	vault_config.set_value("main","version", ProjectSettings.get_setting(APP_VERSION))
+	vault_config.save(vault_settings_file)
+	set_vault_dir(dir)
+	
 
 func print_config(config: ConfigFile):
 	print("Printing Configuration File Contents")
