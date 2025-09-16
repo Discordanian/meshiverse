02b0a25af40115d3f0e93056de19be10df9e262b
Kurt Schwind <kurt.schwind@gmail.com>
2025-09-16 16:12:43 -0500

    dev

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!bash
-export BIN="/Applications/Godot v4.5 rc2.app/Contents/MacOS/Godot"
+export BIN="/Applications/Godot v4.5.app/Contents/MacOS/Godot"
 echo $BIN godot/project.godot
 "$BIN" godot/project.godot
