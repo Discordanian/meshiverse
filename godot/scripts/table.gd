4436ba972f77ac865b6346a50b6f3441a79b6c0c
Kurt Schwind <kurt.schwind@gmail.com>
2025-09-20 16:02:07 -0500

    Snapshot

--- godot/scripts/table.gd
+++ godot/scripts/table.gd
@@ -2,15 +2,30 @@ extends Control
 
 @onready var TableRow = preload("res://scenes/table_row.tscn")
 @onready var TableCell = preload("res://scenes/table_cell.tscn")
+@onready var TableHeader = preload("res://scenes/table_header_cell.tscn")
 
 @export var data: DataFrame
 
 # Called when the node enters the scene tree for the first time.
 func Render() -> void:
     if not data:
-        pass
+        return
+    
+    
         
     var row_count = data.Size()
+    
+    
+    var header_row = TableRow.instantiate()
+    $Rows.add_child(header_row) 
+    
+    for col in data.columns:
+        var cell = TableHeader.instantiate()
+        print("Adding header Cell %s" % col)
+        cell.text = col
+        header_row.add_child(cell)
+        
+    
     for r in range(row_count):
         var row = TableRow.instantiate()
         print("Adding Row to table")
