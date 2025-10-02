aa73742f58e2267ec5868c62181fa37b132a36e1
Kurt Schwind <kurt.schwind@gmail.com>
2025-10-02 08:03:36 -0500

    Start to connect buttons

--- godot/scripts/main.gd
+++ godot/scripts/main.gd
@@ -1,21 +1,25 @@
 extends Control
 
 @onready var table: Node = $MainMargin/VBoxContainer/HBoxContainer/Table
+@onready var menu_bar: Node = $MainMargin/VBoxContainer/MenuBar
 
 func _mock_data(n: int) -> Array[Array]:
-    var data: Array[Array] = []
-    for i: int in range(n):
-        var model_name: String = "Model %d / %d" % [i,n]
-        data.append([i, model_name, 1950+i])
-    return data
+	var data: Array[Array] = []
+	for i: int in range(n):
+		var model_name: String = "Model %d / %d" % [i,n]
+		data.append([i, model_name, 1950+i])
+	return data
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    print("main _ready()")
-    ConfigFileHandler.print_config(ConfigFileHandler.global_config)
-    
-    var columns : PackedStringArray = ["Id","Name","Year"]
-    var data: Array[Array] = _mock_data(200)
-    var df : DataFrame = DataFrame.New(data, columns)
-    table.data = df
-    table.Render()
+	print("main _ready()")
+	ConfigFileHandler.print_config(ConfigFileHandler.global_config)
+	
+	var columns : PackedStringArray = ["Id","Name","Year"]
+	var data: Array[Array] = _mock_data(200)
+	var df : DataFrame = DataFrame.New(data, columns)
+	table.data = df
+	table.Render()
+	
+	var dpi: int = DisplayServer.screen_get_dpi()
+	print("DPI returned is : ", dpi)
