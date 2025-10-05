12356c4820d2b8573dfc6edba910af6b5b8bd543
Kurt Schwind <kurt.schwind@gmail.com>
2025-10-05 08:49:38 -0500

    Snapshot

--- godot/singletons/theme_manager.gd
+++ godot/singletons/theme_manager.gd
@@ -3,14 +3,16 @@ extends Node
 class_name MeshiverseThemeManager
 
 func get_scaled_font_size(base_size: int) -> int:
-    return int(base_size * get_ui_scale_factor())
+    var scaled_factor: float = get_ui_scale_factor()
+    print("Scaled Factor is ", scaled_factor)
+    return int(base_size * scaled_factor)
     
 func get_ui_scale_factor() -> float:
     var screen_size: Vector2 = DisplayServer.screen_get_size()
     var scale_factor: float = DisplayServer.screen_get_max_scale()
     
     print("Screen size: ", screen_size)
-    print("Scale Factor: ", scale_factor)
+    print("Screen Scale Factor: ", scale_factor)
     
     if scale_factor > 1.0:
         return scale_factor
