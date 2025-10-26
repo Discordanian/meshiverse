be8ff461a9b6e02dfd95814b205b48bf0aa93feb
Kurt Schwind <kurt.schwind@gmail.com>
2025-10-26 10:43:24 -0500

    Saving your bacon

--- godot/scripts/main.gd
+++ godot/scripts/main.gd
@@ -11,51 +11,51 @@ enum VisibleScreen { SETTINGS, LIBRARY, DETAILS }
 var visible_screen: VisibleScreen = VisibleScreen.LIBRARY
 
 func setup_theme() -> void:
-	var tm: MeshiverseThemeManager = MeshiverseThemeManager.new()
-	var local_theme: Theme = $".".theme
-	$".".theme = tm.update_theme(local_theme)
+    var tm: MeshiverseThemeManager = MeshiverseThemeManager.new()
+    var local_theme: Theme = $".".theme
+    $".".theme = tm.update_theme(local_theme)
  
 func _handle_menu_bar_signals(button_name: String) -> void:
-	print("handle_menu_bar_signals(%s)" % button_name)
-	match button_name:
-		"SETTINGS":
-			visible_screen = VisibleScreen.SETTINGS
-		"LIBRARY":
-			visible_screen = VisibleScreen.LIBRARY
-		_ :
-			pass
-	_visibility()       
+    print("handle_menu_bar_signals(%s)" % button_name)
+    match button_name:
+        "SETTINGS":
+            visible_screen = VisibleScreen.SETTINGS
+        "LIBRARY":
+            visible_screen = VisibleScreen.LIBRARY
+        _ :
+            pass
+    _visibility()       
 
 func _show_main() -> void:
-	_handle_menu_bar_signals("LIBRARY")
-	
+    _handle_menu_bar_signals("LIBRARY")
+    
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("main _ready()")
-	ConfigFileHandler.print_config(ConfigFileHandler.global_config)
-	
-	if menu_bar:
-		menu_bar.menu_button_pressed.connect(_handle_menu_bar_signals)
-	
-	if settings_node:
-		settings_node.return_to_main.connect(_show_main)
-	
-	# Mock table
-	table.mock(123)
-	table.Render()
+    print("main _ready()")
+    ConfigFileHandler.print_config(ConfigFileHandler.global_config)
+    
+    if menu_bar:
+        menu_bar.menu_button_pressed.connect(_handle_menu_bar_signals)
+    
+    if settings_node:
+        settings_node.return_to_main.connect(_show_main)
+    
+    # Mock table
+    table.mock(123)
+    table.Render()
 
-	setup_theme()
-	_visibility()
+    setup_theme()
+    _visibility()
 
 func _visibility() -> void:
-	match visible_screen:
-		VisibleScreen.SETTINGS:
-			primary_node.visible = false
-			settings_node.visible = true
-		VisibleScreen.LIBRARY:
-			primary_node.visible = true
-			settings_node.visible = false           
-		VisibleScreen.DETAILS:
-			print("Not currently able to handle DETAILS screen")
-		_:
-			push_error("Unspecified VisibleScreen state in visibility() function %s" % VisibleScreen.keys()[visible_screen]) 
+    match visible_screen:
+        VisibleScreen.SETTINGS:
+            primary_node.visible = false
+            settings_node.visible = true
+        VisibleScreen.LIBRARY:
+            primary_node.visible = true
+            settings_node.visible = false           
+        VisibleScreen.DETAILS:
+            print("Not currently able to handle DETAILS screen")
+        _:
+            push_error("Unspecified VisibleScreen state in visibility() function %s" % VisibleScreen.keys()[visible_screen]) 
